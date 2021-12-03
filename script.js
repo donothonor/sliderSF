949b37c981181f10f1fcbc69f2ac5b68ae6f3aa5
VADIM <nagiozv2@gmail.com>
2021-12-03 14:41:54 +0400

    complete slider

--- script.js
+++ script.js
@@ -2,21 +2,36 @@ const navi = document.querySelectorAll('.navigation-item')
 const prev = document.querySelector('.l-arrow')
 const next = document.querySelector('.r-arrow')
 const image = document.querySelector('.image')
+const dots = document.querySelectorAll('.dot')
 
-const pictures = ["https://i.postimg.cc/pTD9hMrR/image-2.png","https://i.postimg.cc/L5tzpZLk/image-2-1.png","https://i.postimg.cc/4dC7xxJW/image-3.png"]
+const pictures = ["https://i.postimg.cc/pTD9hMrR/image-2.png",
+                  "https://i.postimg.cc/L5tzpZLk/image-2-1.png",
+                  "https://i.postimg.cc/4dC7xxJW/image-3.png"]
 
-navi.forEach(naviItem => {
+navi.forEach((naviItem, naviIndex) => {
     naviItem.addEventListener('click', e => {
         e.preventDefault()
+        defaultCondition()
+        moveSlide(naviIndex)
     })
 })
+dots.forEach((dotItem, dotIndex) => {
+    dotItem.addEventListener('click', () => {
+        defaultCondition()
+        moveSlide(dotIndex)
+    })
+})
+
+
 let currentIndex = 0
+
 prev.addEventListener('click', () => {
     if (!currentIndex) {
         currentIndex = pictures.length - 1 
     } else {
         currentIndex--
     }
+    defaultCondition()
     moveSlide(currentIndex)
 })
 next.addEventListener('click', () => {
@@ -25,13 +40,27 @@ next.addEventListener('click', () => {
     } else {
         currentIndex++
     }
+    defaultCondition()
     moveSlide(currentIndex)
 })
 
 function moveSlide (index) {
     image.style.backgroundImage = `url(${pictures[index]})`
+    navi[index].classList.add('active-link')
+    dots[index].style.backgroundColor = 'white'
+    currentIndex = index
 }
 
-
+document.addEventListener('DOMContentLoaded', () => {
+    moveSlide(0)
+})
+function defaultCondition () {
+    navi.forEach(nav => {
+        nav.classList.remove('active-link')
+    })
+    dots.forEach(dot => {
+        dot.style.backgroundColor = 'rgba(255, 255, 255, 0.3)'
+    })
+}
 
 
